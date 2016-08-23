f6f5ac5f82d7935a4e6f7d743477191bf185d167
i2ft <fordivil1@gmail.com>
2016-08-23 16:15:50 +0900

    a

--- post/views.py
+++ post/views.py
@@ -2,19 +2,26 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .form import PostForm
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from .models import Post
-from .service import setDep
+from .service import setDep,reset_dep
 from django.core import serializers
 
 # Create your views here.
 def post_update(request, slug=None):
 	if not request.user.is_staff or not request.user.is_superuser:
 		raise Http404
+
 	instance = get_object_or_404(Post, slug=slug)
 	form = PostForm(request.POST or None, request.FILES or None, instance=instance)
 	if form.is_valid():
 		instance = form.save(commit=False)
+		if (form.cleaned_data["parent"] == "NULL"):
+			reset_dep(instance);
+		if (form.cleaned_data["parent"] != "NULL"):
+			setDep(instance);
+			reset_dep(instance);
+
 		instance.save()
-		#messages.success(request, "<a href='#'>Item</a> Saved", extra_tags='html_safe')
+
 		return HttpResponseRedirect(instance.get_absolute_url())
 
 	context = {
